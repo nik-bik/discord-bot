eb6a799ae292e56716a136b2d7df335301ceca35
nikbik <63609922+ItzJustas@users.noreply.github.com>
2022-06-03 17:01:51 +0300

    Update search engine.py

--- thingys py idk/search engine.py
+++ thingys py idk/search engine.py
@@ -1,10 +1,10 @@
-api_key = "AIzaSyBRK-61Y-rcHCZDA0M6i58eeV0tsdSOfJA"
+api_key = ""
 @client.command(aliases=["show"])
 async def showpic(ctx, *, search):
     ran = random.randint(0, 9)
     resource = build("customsearch", "v1", developerKey=api_key).cse()
     result = resource.list(
-        q=f"{search}", cx="de90d356f1ec3f07f", searchType="image").execute()
+        q=f"{search}", cx="", searchType="image").execute()
     url = result["items"][ran]["link"]
     embed1 = discord.Embed(title=f"Here Your Image ({search})")
     embed1.set_image(url=url)
