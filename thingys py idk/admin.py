624b47daa7420ef8658661659c93da593683f48c
nikbik <63609922+ItzJustas@users.noreply.github.com>
2022-06-03 23:32:05 +0300

    Update admin.py

--- thingys py idk/admin.py
+++ thingys py idk/admin.py
@@ -21,3 +21,17 @@ async def kick(ctx, member: discord.Member, *, reason=None):
 async def ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f"banned {member.mention}, he was a miene ;~; ")
+    
+ 
+# unban command #
+@client.command()
+async def unban(ctx, *, member):
+    banned_users = await ctx.guild.bans()
+
+    member_name, member_discriminator = member.split('#')
+    for ban_entry in banned_users:
+        user = ban_entry.user
+
+        if (user.name, user.discriminator) == (member_name, member_discriminator):
+            await ctx.guild.unban(user)
+            await ctx.channel.send(f"Unbanned: {user.mention}")
