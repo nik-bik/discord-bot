6821cfdae683cb43f3dd0137c4ef16818b26f1b7
nikbik <63609922+ItzJustas@users.noreply.github.com>
2022-07-10 11:45:54 +0100

    Update pp.py
    fancy pp

--- thingys py idk/pp.py
+++ thingys py idk/pp.py
@@ -24,4 +24,6 @@ async def pp(ctx, member: discord.Member, *, reason=None):
                  "8===============================================================================D",
                  "(i)"
                  ]
-    await ctx.send(f"{member.mention} {random.choice(responses)}")
+    pp = discord.Embed(title=f"PP Size of {member.name}#{member.discriminator}")
+    pp.add_field(name="Your PP size is ", value=random.choice(responses))
+    await ctx.send(embed=pp)
